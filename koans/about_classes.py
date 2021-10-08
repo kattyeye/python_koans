12cd0ccb42dda51a47ac3406c1f5cc98ad3e656c
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-10-08 13:29:37 -0400

    Adding work on classes

--- koans/about_classes.py
+++ koans/about_classes.py
@@ -3,6 +3,8 @@
 
 from runner.koan import *
 
+# https://www.programiz.com/python-programming/class
+
 
 class AboutClasses(Koan):
     class Dog:
@@ -12,7 +14,8 @@ class AboutClasses(Koan):
         # NOTE: The .__name__ attribute will convert the class
         # into a string value.
         fido = self.Dog()
-        self.assertEqual(__, fido.__class__.__name__)
+        self.assertEqual(
+            'Dog', fido.__class__.__name__)
 
     def test_classes_have_docstrings(self):
         self.assertRegex(self.Dog.__doc__, __)
@@ -28,12 +31,12 @@ class AboutClasses(Koan):
 
     def test_init_method_is_the_constructor(self):
         dog = self.Dog2()
-        self.assertEqual(__, dog._name)
+        self.assertEqual('Paul', dog._name)
 
     def test_private_attributes_are_not_really_private(self):
         dog = self.Dog2()
         dog.set_name("Fido")
-        self.assertEqual(__, dog._name)
+        self.assertEqual("Fido", dog._name)
         # The _ prefix in _name implies private ownership, but nothing is truly
         # private in Python.
 
@@ -41,11 +44,11 @@ class AboutClasses(Koan):
         fido = self.Dog2()
         fido.set_name("Fido")
 
-        self.assertEqual(__, getattr(fido, "_name"))
+        self.assertEqual("Fido", getattr(fido, "_name"))
         # getattr(), setattr() and delattr() are a way of accessing attributes
         # by method rather than through assignment operators
 
-        self.assertEqual(__, fido.__dict__["_name"])
+        self.assertEqual(["Paul", "Fido"], fido.__dict__["_name"])
         # Yes, this works here, but don't rely on the __dict__ object! Some
         # class implementations use optimization which result in __dict__ not
         # showing everything.
@@ -69,10 +72,10 @@ class AboutClasses(Koan):
         fido.set_name("Fido")
 
         # access as method
-        self.assertEqual(__, fido.get_name())
+        self.assertEqual("Fido", fido.get_name())
 
         # access as property
-        self.assertEqual(__, fido.name)
+        self.assertEqual("Fido", fido.name)
 
     # ------------------------------------------------------------------
 
@@ -92,7 +95,7 @@ class AboutClasses(Koan):
         fido = self.Dog4()
 
         fido.name = "Fido"
-        self.assertEqual(__, fido.name)
+        self.assertEqual("Fido", fido.name)
 
     # ------------------------------------------------------------------
 
@@ -106,10 +109,10 @@ class AboutClasses(Koan):
 
     def test_init_provides_initial_values_for_instance_variables(self):
         fido = self.Dog5("Fido")
-        self.assertEqual(__, fido.name)
+        self.assertEqual("Fido", fido.name)
 
     def test_args_must_match_init(self):
-        with self.assertRaises(___):
+        with self.assertRaises(TypeError):
             self.Dog5()
 
         # THINK ABOUT IT:
@@ -119,7 +122,7 @@ class AboutClasses(Koan):
         fido = self.Dog5("Fido")
         rover = self.Dog5("Rover")
 
-        self.assertEqual(__, rover.name == fido.name)
+        self.assertEqual(False, rover.name == fido.name)
 
     # ------------------------------------------------------------------
 
