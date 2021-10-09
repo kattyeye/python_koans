e2fb64d9db1a2bb82884e764bd41a44ac63c1d22
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-10-09 12:55:17 -0400

    Adding monkey patching

--- koans/about_monkey_patching.py
+++ koans/about_monkey_patching.py
@@ -7,6 +7,7 @@
 
 from runner.koan import *
 
+
 class AboutMonkeyPatching(Koan):
     class Dog:
         def bark(self):
@@ -14,7 +15,7 @@ class AboutMonkeyPatching(Koan):
 
     def test_as_defined_dogs_do_bark(self):
         fido = self.Dog()
-        self.assertEqual(__, fido.bark())
+        self.assertEqual("WOOF", fido.bark())
 
     # ------------------------------------------------------------------
 
@@ -24,10 +25,12 @@ class AboutMonkeyPatching(Koan):
         self.Dog.wag = wag
 
         fido = self.Dog()
-        self.assertEqual(__, fido.wag())
-        self.assertEqual(__, fido.bark())
+        self.assertEqual("HAPPY", fido.wag())
+        self.assertEqual("WOOF", fido.bark())
 
     # ------------------------------------------------------------------
+# A MonkeyPatch is a piece of Python code which extends or modifies other code at runtime (typically at startup).
+# https://stackoverflow.com/questions/5626193/what-is-monkey-patching
 
     def test_most_built_in_classes_cannot_be_monkey_patched(self):
         try:
@@ -35,14 +38,15 @@ class AboutMonkeyPatching(Koan):
         except Exception as ex:
             err_msg = ex.args[0]
 
-        self.assertRegex(err_msg, __)
+        self.assertRegex(err_msg, "ex")
 
     # ------------------------------------------------------------------
 
-    class MyInt(int): pass
+    class MyInt(int):
+        pass
 
     def test_subclasses_of_built_in_classes_can_be_be_monkey_patched(self):
         self.MyInt.is_even = lambda self: (self % 2) == 0
 
-        self.assertEqual(__, self.MyInt(1).is_even())
-        self.assertEqual(__, self.MyInt(2).is_even())
+        self.assertEqual(False, self.MyInt(1).is_even())
+        self.assertEqual(True, self.MyInt(2).is_even())
